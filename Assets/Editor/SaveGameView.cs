0b7efc713013e1fc908b120d4648587b1b0e5a31
John Einselen VF <jeinselen@vectorform.com>
2023-05-24 18:16:13 -0400

    Initial code commit

---
+++ Assets/Editor/SaveGameView.cs
@@ -0,0 +1,162 @@
+using System.IO;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SaveGameView : EditorWindow
+{
+	private enum MultiSampleList
+	{
+		_Disabled = 0,
+		_2x = 2,
+		_4x = 4,
+		_8x = 8
+	}
+	
+	// Script variables
+	[SerializeField, Tooltip("Location for saving PNG files")]
+	private string filePath;
+	[SerializeField, Tooltip("PNG file name, dynamic variables: {scene} {camera} {samples} {date} {time}")]
+	private string fileName = "{scene} {camera} {samples} {date} {time}";
+	private MultiSampleList multiSampleSelected;
+	private Camera activeCamera;
+	
+	// Add panel to the Unity menu system
+	[MenuItem("Tools/Vectorform/Save Game View")]
+	public static void ShowWindow()
+	{
+//		EditorWindow.GetWindow(typeof(SaveGameView));
+		SaveGameView window = (SaveGameView)EditorWindow.GetWindow(typeof(SaveGameView));
+		window.titleContent = new GUIContent("Save Game View");
+	}
+	
+	private void OnGUI()
+	{
+		GUILayout.Space(10);
+		
+		// File location
+		EditorGUILayout.BeginHorizontal();
+		filePath = EditorGUILayout.TextField("File Path", filePath);
+		
+		if (filePath.Length <= 0 || !System.IO.Directory.Exists(filePath))
+		filePath = Application.dataPath.Replace("Assets", "Renders");
+		
+		if (GUILayout.Button("browse", GUILayout.MaxWidth(64)))
+		{
+			filePath = EditorUtility.SaveFolderPanel("File path", filePath, "");
+		}
+		
+		GUI.enabled = Directory.Exists(filePath);
+		if (GUILayout.Button("reveal", GUILayout.MaxWidth(64)))
+		{
+			EditorUtility.RevealInFinder(filePath);
+		}
+		GUI.enabled = true;
+		
+		if (filePath.Length > 0 && filePath[filePath.Length - 1] != '/')
+		{
+			filePath += '/';
+		}
+		
+		EditorGUILayout.EndHorizontal();
+		
+		// File name
+		fileName = EditorGUILayout.TextField("File Name", fileName);
+		
+		// Multi sampling dropdown menu
+		multiSampleSelected = (MultiSampleList)EditorGUILayout.EnumPopup("Anti Aliasing (MSAA)", multiSampleSelected);
+		
+		GUILayout.Space(10);
+		
+		if (GUILayout.Button("Save PNG with Alpha"))
+		{
+			CaptureGameView();
+		}
+		
+		GUILayout.Space(10);
+		
+		GUILayout.Label("Scene:  " + (SceneManager.GetActiveScene() != null ? SceneManager.GetActiveScene().name : "None"));
+		GUILayout.Label("Camera:  " + (Camera.main != null ? Camera.main.name : "None"));
+//		int multiSampleValue = (int)multiSampleSelected;
+//		GUILayout.Label("Samples:  " + (multiSampleValue != null ? multiSampleValue.ToString() : "None"));
+//		EditorGUILayout.LabelField("Samples:  ", multiSampleValue.ToString());
+	}
+	
+	private void CaptureGameView()
+	{
+		// Open game window if not in play mode
+		if (!EditorApplication.isPlaying)
+		{
+			EditorApplication.ExecuteMenuItem("Window/General/Game");
+		}
+		
+		// Get the current active camera from the scene view
+		Camera activeCamera = Camera.main;
+		
+		// Check if a camera is present
+		if (activeCamera == null)
+		{
+			Debug.Log("No active camera found in the scene view.");
+			return;
+		}
+		
+		// Get multi sampling value
+		int multiSampleValue = (int)multiSampleSelected;
+		
+		// Process file name and combine with file path
+		string nameTemp = fileName;
+		nameTemp = nameTemp.Replace("{scene}", SceneManager.GetActiveScene().name);
+		nameTemp = nameTemp.Replace("{camera}", activeCamera.name);
+		nameTemp = nameTemp.Replace("{samples}", "MSAA" + multiSampleValue);
+		nameTemp = nameTemp.Replace("{date}", System.DateTime.Now.ToString("yyyy-MM-dd"));
+		nameTemp = nameTemp.Replace("{time}", System.DateTime.Now.ToString("H-mm-ss.f"));
+		string finalPath = filePath + "/" + nameTemp + ".png";
+		
+		// Get the width/height of the game view
+		Vector2 resolution = GetMainGameViewSize();
+		int width = (int)resolution.x;// - 10;
+		int height = (int)resolution.y;// - 10;
+				
+		// Create a render texture with the same dimensions as the game view, including alpha
+		RenderTexture renderTexture = new RenderTexture(width, height, 24, RenderTextureFormat.ARGB32);
+		if (multiSampleValue > 0)
+		{
+			renderTexture.antiAliasing = multiSampleValue;
+		}
+		
+		// Set the render texture target and render the game view
+		activeCamera.targetTexture = renderTexture;
+		activeCamera.Render();
+		RenderTexture.active = renderTexture;
+		
+		// Create output texture and read the pixels from the render texture
+		Texture2D screenshotTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+		screenshotTexture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+		screenshotTexture.Apply();
+		
+		// Reset the active render texture and camera target texture, remove the render texture
+		activeCamera.targetTexture = null;
+		RenderTexture.active = null;
+		DestroyImmediate(renderTexture);
+		
+		// Convert the texture 2D to PNG bytes, remove the temporary texture, and save the bytes to a PNG file
+		byte[] pngData = screenshotTexture.EncodeToPNG();
+		DestroyImmediate(screenshotTexture);
+		File.WriteAllBytes(finalPath, pngData);
+		
+		// Provide feedback in the console
+//		Debug.Log("Game view captured and exported to: " + finalPath);
+	}
+	
+	// Returns game view resolution even if that panel is not focused
+	private Vector2 GetMainGameViewSize()
+	{
+		System.Type T = System.Type.GetType("UnityEditor.GameView,UnityEditor");
+		System.Reflection.MethodInfo GetSizeOfMainGameView = T.GetMethod("GetSizeOfMainGameView", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+		System.Object Res = GetSizeOfMainGameView.Invoke(null, null);
+		return (Vector2)Res;
+	}
+}
---
+++ Assets/Scripts/SaveCameraView.cs
@@ -0,0 +1,197 @@
+using System.IO;
+using System.Collections;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEngine.Experimental.Rendering;
+
+[CustomEditor(typeof(SaveCameraView))]
+public class SaveCameraViewEditor : Editor
+{
+	private SerializedProperty filePathProperty;
+	private SerializedProperty fileNameProperty;
+	private SerializedProperty multiSampleProperty;
+	
+	private void OnEnable()
+	{
+		filePathProperty = serializedObject.FindProperty("filePath");
+		fileNameProperty = serializedObject.FindProperty("fileName");
+		multiSampleProperty = serializedObject.FindProperty("multiSample");
+	}
+	
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+		
+		SaveCameraView saveCameraView = (SaveCameraView)target; // What is this doing?
+				
+		// File path input field
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.PropertyField(filePathProperty);
+		
+		if (filePathProperty.stringValue.Length <= 0 || !System.IO.Directory.Exists(filePathProperty.stringValue))
+		filePathProperty.stringValue = Application.dataPath.Replace("Assets", "Renders");
+		
+		if (GUILayout.Button("browse", GUILayout.MaxWidth(64)))
+		{
+			filePathProperty.stringValue = EditorUtility.SaveFolderPanel("Render path", filePathProperty.stringValue, "");
+		}
+		
+		GUI.enabled = Directory.Exists(filePathProperty.stringValue);
+		if (GUILayout.Button("reveal", GUILayout.MaxWidth(64)))
+		{
+			EditorUtility.RevealInFinder(filePathProperty.stringValue);
+		}
+		GUI.enabled = true;
+		
+		if (filePathProperty.stringValue.Length > 0 && filePathProperty.stringValue[filePathProperty.stringValue.Length - 1] != '/')
+		{
+			filePathProperty.stringValue += '/';
+		}
+		
+		EditorGUILayout.EndHorizontal();
+		// End file path input field
+		
+		// File name input field
+		EditorGUILayout.PropertyField(fileNameProperty);
+		
+		// Multi sampling dropdown menu
+		EditorGUILayout.PropertyField(multiSampleProperty);
+//		var randomGunGenerator = target as RandomGunGenerator;
+//		randomGunGenerator.gunType = (RandomGunGenerator.GunTypes)EditorGUILayout.EnumPopup("Type Of Gun:", randomGunGenerator.gunType);
+		
+		serializedObject.ApplyModifiedProperties();
+		
+		EditorGUILayout.Space();
+		
+		if (GUILayout.Button("Save PNG with Alpha"))
+		{
+			saveCameraView.CaptureCameraView();
+		}
+	}
+}
+
+[ExecuteInEditMode]
+public class SaveCameraView : MonoBehaviour
+{
+	private enum MultiSampleList
+	{
+		_Disabled = 0,
+		_2x = 2,
+		_4x = 4,
+		_8x = 8
+	}
+	
+	[SerializeField, Tooltip("Location for saving PNG files")]
+	private string filePath;
+	[SerializeField, Tooltip("PNG file name, dynamic variables: {scene} {camera} {date} {time}")]
+	private string fileName = "{scene} {camera} {date} {time}";
+	[SerializeField, Tooltip("Anti Aliasing (MSAA)")]
+	private MultiSampleList multiSample;
+	private Camera activeCamera;
+	
+	private bool hideGUI;
+	
+	// On component reset
+	private void Reset()
+	{
+		filePath = Application.dataPath.Replace("Assets", "Renders");
+		fileName = "{scene} {camera} {date} {time}";
+	}
+	
+	// Triggered from the object inspector GUI
+	public void CaptureCameraView()
+	{
+		if (!gameObject.activeInHierarchy)
+		{
+			Debug.Log("Selected camera is inactive");
+			return;
+		}
+		
+		// Get multi sampling value
+		int multiSampleValue = (int)multiSample;
+		
+		// Get the local camera component
+		activeCamera = GetComponent<Camera>();
+//		commandBuffer = new CommandBuffer();
+//		activeCamera.AddCommandBuffer(CameraEvent.AfterEverything, commandBuffer);
+		
+		// Getting the width/height of the game view even when not active
+		Vector2 resolution = GetMainGameViewSize();
+		int width = (int)resolution.x;
+		int height = (int)resolution.y;
+		
+		// Create a render texture with the same dimensions as the game view, including alpha
+		// ARGB32 - works only without post processing, ARGBHalf and ARGBFloat both have wrong gamma
+		// https://docs.unity3d.com/2022.1/Documentation/ScriptReference/RenderTextureFormat.html
+		// https://docs.unity3d.com/ScriptReference/Experimental.Rendering.GraphicsFormat.html
+		RenderTexture renderTexture = new RenderTexture(width, height, 32, RenderTextureFormat.ARGBFloat);
+//		RenderTexture renderTexture = new RenderTexture(width, height, 32, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.sRGB);
+//		RenderTexture renderTexture = RenderTexture.GetTemporary(width, height, 32, RenderTextureFormat.ARGBFloat);
+		if (multiSampleValue > 0)
+		{
+			renderTexture.antiAliasing = multiSampleValue;
+		}
+		
+		// Render the game view to the render texture
+		activeCamera.targetTexture = renderTexture;
+		activeCamera.Render();
+		
+		// Read the pixels from the render texture into a texture 2D
+		// https://docs.unity3d.com/2022.1/Documentation/ScriptReference/TextureFormat.html
+		RenderTexture.active = renderTexture;
+		Texture2D screenshotTexture = new Texture2D(width, height, TextureFormat.RGBAFloat, false);
+//		Texture2D screenshotTexture = new Texture2D(width, height, TextureFormat.RGBAHalf, false);
+//		Texture2D screenshotTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+		screenshotTexture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+		screenshotTexture.Apply();
+		
+		// Convert linear render data to sRGB through a stupid complicated process where we shuffle everything
+		// Replace render texture with 8-bit version (should default to sRGB?)
+		renderTexture = RenderTexture.GetTemporary(width, height, 24, RenderTextureFormat.ARGB32);
+		// Copy the floating point texture to the 8-bit render texture
+		Graphics.Blit(screenshotTexture, renderTexture);
+		// Set render texture as active
+		RenderTexture.active = renderTexture;
+		// Replace output texture with 8-bit version (should default to sRGB?)
+		screenshotTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+		// Replace contents. Again.
+		screenshotTexture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+		// Amazingly, this works, and is apparently the process required to fix PNG output gamma...what a nightmare
+		screenshotTexture.Apply();
+		
+		// Reset the active render texture and camera target texture
+		RenderTexture.active = null;
+		activeCamera.targetTexture = null;
+		DestroyImmediate(renderTexture);
+//		RenderTexture.ReleaseTemporary(renderTexture);
+		
+		// Process file name and combine with file path
+		string nameTemp = fileName;
+		nameTemp = nameTemp.Replace("{scene}", SceneManager.GetActiveScene().name);
+		nameTemp = nameTemp.Replace("{camera}", activeCamera.name);
+		nameTemp = nameTemp.Replace("{samples}", "MSAA" + multiSampleValue);
+		nameTemp = nameTemp.Replace("{date}", System.DateTime.Now.ToString("yyyy-MM-dd"));
+		nameTemp = nameTemp.Replace("{time}", System.DateTime.Now.ToString("H-mm-ss.f"));
+		string finalPath = filePath + "/" + nameTemp + ".png";
+		
+		// Convert the Texture2D to PNG bytes and remove the source
+		byte[] pngData = screenshotTexture.EncodeToPNG();
+		DestroyImmediate(screenshotTexture);
+		
+		// Save the PNG bytes to a file
+		File.WriteAllBytes(finalPath, pngData);
+		
+		// Provide feedback in the console
+		Debug.Log("Game view captured and exported to: " + finalPath);
+	}
+	
+	// Returns game view resolution even if that panel is not focused
+	private Vector2 GetMainGameViewSize()
+	{
+		System.Type T = System.Type.GetType("UnityEditor.GameView,UnityEditor");
+		System.Reflection.MethodInfo GetSizeOfMainGameView = T.GetMethod("GetSizeOfMainGameView", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+		System.Object Res = GetSizeOfMainGameView.Invoke(null, null);
+		return (Vector2)Res;
+	}
+}
